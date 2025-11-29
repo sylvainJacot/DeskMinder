ff68714eb6845e7d149cb6efd31d61e6d76f5cd4
Sylvain <jct.sylvain@gmail.com>
2025-11-29 21:27:15 +0100

    Initial Commit

---
+++ DeskMinder/DeskMinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskMinderApp.swift
+//  DeskMinder
+//
+//  Created by Sylvain Jacot on 29/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct DeskMinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
